d1799d84db3aadf7094aa68fe9cada45c1f48bf7
James Kachiro Sarumaha <james.kachiro@sarumaha.com>
2023-05-01 13:25:19 +0700

    Styling Header and Add Logo

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,9 +1,31 @@
 // components/header.js
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "./../assets/logo.png";
 
 export default function Header() {
     return (
         <header>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <div className="left">
+                        <div className="row logo-wrapper">
+                            <Image src={Logo} className="logo" alt="logo cantomon" />
+                        </div>
+                        <div className="row menu-wrapper">
+                            <Link className='main menu' href='#'><i className='bi bi-list'></i></Link>
+                            <Link className='info menu' href='#'><i className='bi bi-info'></i></Link>
+                        </div>
+                    </div>
+                    <div className="center">
+                         
+                    </div>
+                    <div className="right">
+                        <button className="btn btn-primary">Wallet Connection</button>
+                    </div>
+                </div>
+            </nav>
         </header>
     )
 }
