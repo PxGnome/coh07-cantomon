b140223fb298c71268819f2cef6452d02b3a7759
James Kachiro Sarumaha <james.kachiro@sarumaha.com>
2023-05-01 10:57:24 +0700

    [Frontend] Enable Sass

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+
+import '../styles/customBootstrap.scss';
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
