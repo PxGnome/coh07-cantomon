233919b9d09effd0e4ba1d45a3b9b1d771c79e44
James Kachiro Sarumaha <james.kachiro@sarumaha.com>
2023-05-02 15:43:38 +0700

    Enabling bootstrap icons

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,6 +4,7 @@ import Layout from '@/components/layout';
 import store from '@/utils/store';
 import { Provider } from "react-redux";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
 import '../styles/customBootstrap.scss';
 import '@/styles/globals.scss';
 
