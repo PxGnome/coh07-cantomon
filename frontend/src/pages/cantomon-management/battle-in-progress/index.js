a25ce19e80c6a950327cac714d6994aec12ab921
James Kachiro Sarumaha <james.kachiro@sarumaha.com>
2023-05-06 22:01:12 +0700

    Add full animation of battle-in-progress

--- frontend/src/pages/cantomon-management/battle-in-progress/index.js
+++ frontend/src/pages/cantomon-management/battle-in-progress/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-import Head from 'next/head';   
+import Head from 'next/head';
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentPage } from '@/features/manageNavigationState';
-import dynamic from 'next/dynamic'; 
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const DynamicComponentWithNoSSR = dynamic(
     () => import('@/phaser/battle-in-progress/app'),
@@ -12,12 +13,24 @@ const DynamicComponentWithNoSSR = dynamic(
 )
 export default function CantomonManagementBattleInProgress() {
 
+    
+    const [visible, setVisible] = React.useState(false);
     const [loading, setLoading] = useState(false);
-    const router = useRouter(); 
 
-    const dispatch = useDispatch(); 
-    const current_page = useSelector((state) => state?.navStore.current_page); 
+ 
+    const closeModal = () => {
+       setVisible(false); 
+    }
+    const goBack = () => {
+        router.push("/cantomon-management/battle");
+    }
+    const router = useRouter();
 
+    const dispatch = useDispatch();
+    const current_page = useSelector((state) => state?.navStore.current_page);
+    const showModal = () => {
+        setVisible(true); 
+     }
     useEffect(() => {
         if (router.isReady) {
 
@@ -29,19 +42,44 @@ export default function CantomonManagementBattleInProgress() {
 
     React.useEffect(() => {
         console.log("current_page -> ", current_page);
-        if(current_page == "" || current_page != "/cantomon-management/battle-in-progress"){
+        if (current_page == "" || current_page != "/cantomon-management/battle-in-progress") {
             dispatch(setCurrentPage({
                 current_page: "/cantomon-management/battle-in-progress"
             }))
         }
-    },[])
+    }, [])
     return (
         <>
-            <Head>
-                <title>Battle In Progress | Cantomon</title>
-            </Head> 
-            <div key={Math.random()} id="battlescene-container" className='battle-scene'></div>
-            {loading && (<DynamicComponentWithNoSSR test="test" />)}
+            <div id='battlesecene-main'>
+                <Head>
+                    <title>Battle In Progress | Cantomon</title>
+                </Head>
+                <div key={Math.random()} id="battlescene-container" className='battle-scene'>
+                </div>
+                {loading && (<DynamicComponentWithNoSSR />)}
+                {loading && (<div className='btn result'><button onClick={showModal}>Result</button></div>)}
+
+            </div>
+            <div className={visible ? ("modal show") : ("modal")}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Window</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"
+
+                                onClick={closeModal}></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Battle Result</p>
+                        </div>
+                        <div className="modal-footer center">
+                            <button type="button" className="btn btn-primary"
+
+                                onClick={goBack}>Go Back</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- frontend/src/phaser/battle-in-progress/battle_scene.js
+++ frontend/src/phaser/battle-in-progress/battle_scene.js
@@ -3,6 +3,8 @@ import 'phaser';
 export default class Demo3Scene extends Phaser.Scene {
     constructor(args) {
         super();
+        window.game = this;
+        window.isCollide = false;
         this.systemdata = args;
         try {
             this.player = this.systemdata.selected_cantomon.image.src;
@@ -14,9 +16,22 @@ export default class Demo3Scene extends Phaser.Scene {
 
     }
 
-    preload() { 
-        
-        this.load.spritesheet('cloud', "/battle-in-progress/battlecloudsheet.png", { frameWidth: 150, frameHeight: 125 });
+    preload() {
+
+        this.load.spritesheet('cloud', "/battle-in-progress/battlecloudsheet.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-1', "/battle-in-progress/cloud-1.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-2', "/battle-in-progress/cloud-2.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-3', "/battle-in-progress/cloud-3.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-4', "/battle-in-progress/cloud-4.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-5', "/battle-in-progress/cloud-5.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-6', "/battle-in-progress/cloud-6.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-7', "/battle-in-progress/cloud-7.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-8', "/battle-in-progress/cloud-8.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-9', "/battle-in-progress/cloud-9.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-10', "/battle-in-progress/cloud-10.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-11', "/battle-in-progress/cloud-11.png", { frameWidth: 145, frameHeight: 144 });
+        this.load.spritesheet('cloud-12', "/battle-in-progress/cloud-12.png", { frameWidth: 145, frameHeight: 144 });
+
         this.load.image('bg', "/battle-in-progress/battle-arena.png");
         if (this.player != "" && this.enemy != "") {
             this.load.image('player', this.player);
@@ -26,6 +41,7 @@ export default class Demo3Scene extends Phaser.Scene {
 
     create() {
         //  The background  
+        window.gamex = this;
         this.add.image(320, 0, 'bg').setOrigin(0.5, 0);
         if (this.player != "" && this.player != "") {
             const player = this.add.image(70, 237, 'player').setOrigin(0.5, 0);
@@ -33,7 +49,7 @@ export default class Demo3Scene extends Phaser.Scene {
             const playerFighting = this.add.image(235, 190, 'player').setOrigin(0.5, 0);
             const enemyFighting = this.add.image(389, 190, 'enemy').setOrigin(0.5, 0);
             this.playerFighting = playerFighting;
-            this.enemyFighting = enemyFighting; 
+            this.enemyFighting = enemyFighting;
             this.tweens.add({
                 targets: this.playerFighting,
                 x: 305,
@@ -55,24 +71,52 @@ export default class Demo3Scene extends Phaser.Scene {
             player.setScale(1.8);
             enemy.setScale(1.8);
 
-            this.physics.world.enable(this.playerFighting); 
-            //this.playerFighting.body.setCollideWorldBounds(true);
-            this.physics.world.enable(this.enemyFighting); 
-            //this.enemyFighting.body.setCollideWorldBounds(true);
-           
-          
-            this.cloud = this.add.sprite(310, 205, 'cloud');
-            this.cloud.setScale(0.6);
-            this.cloud.setVisible(false); 
-            
-        
+            this.anims.create({
+                key: 'explode',
+                frames: [
+                    { key: 'cloud-1' },
+                    { key: 'cloud-2' },
+                    { key: 'cloud-3' },
+                    { key: 'cloud-4' },
+                    { key: 'cloud-5' },
+                    { key: 'cloud-6' },
+                    { key: 'cloud-7' },
+                    { key: 'cloud-8' },
+                    { key: 'cloud-9' },
+                    { key: 'cloud-10' },
+                    { key: 'cloud-11' },
+                    { key: 'cloud-12' },
+                ],
+                frameRate: 60,
+                repeat: -1
+            });
+
+            this.physics.world.enable(this.playerFighting);
+            this.playerFighting.body.setCollideWorldBounds(true);
+            this.physics.world.enable(this.enemyFighting);
+            this.enemyFighting.body.setCollideWorldBounds(true);
+            window.playerFighting = this.playerFighting;
+            window.enemyFighting = this.enemyFighting;
+
+            window.cloud = window.gamex.add.sprite(295, 210, 'cloud');
+            window.cloud.setScale(0.8);
+            window.explodeAnim = window.cloud.play("explode", false);
+            window.cloud.setVisible(false);
+
+            this.physics.add.overlap(this.playerFighting, this.enemyFighting, function (gameObject1, gameObject2) {
+                if (Phaser.Math.Within(gameObject1.body.center.x, gameObject2.body.center.x, 14)) {
+                    window.cloud.setVisible(true);
+                } else {
+                    window.cloud.setVisible(false);
+                }
+            });
+            window.isCollide = false;
+
+
+
         }
 
     }
     update() {
-        this.physics.add.collider(this.playerFighting, this.enemyFighting, function(gameObject1, gameObject2) {
-              
-            
-        }); 
     }
 }
