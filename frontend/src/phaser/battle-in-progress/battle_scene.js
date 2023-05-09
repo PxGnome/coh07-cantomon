b3529987e5f2862541bdfd24a0dfc904238039be
James Kachiro Sarumaha <james.kachiro@sarumaha.com>
2023-05-09 17:02:30 +0700

    change cloudsheet, change font family and resize component to be fit with the new font family

--- frontend/src/phaser/battle-in-progress/battle_scene.js
+++ frontend/src/phaser/battle-in-progress/battle_scene.js
@@ -18,20 +18,7 @@ export default class Demo3Scene extends Phaser.Scene {
 
     preload() {
 
-        this.load.spritesheet('cloud', "/battle-in-progress/battlecloudsheet.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-1', "/battle-in-progress/cloud-1.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-2', "/battle-in-progress/cloud-2.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-3', "/battle-in-progress/cloud-3.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-4', "/battle-in-progress/cloud-4.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-5', "/battle-in-progress/cloud-5.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-6', "/battle-in-progress/cloud-6.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-7', "/battle-in-progress/cloud-7.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-8', "/battle-in-progress/cloud-8.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-9', "/battle-in-progress/cloud-9.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-10', "/battle-in-progress/cloud-10.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-11', "/battle-in-progress/cloud-11.png", { frameWidth: 145, frameHeight: 144 });
-        this.load.spritesheet('cloud-12', "/battle-in-progress/cloud-12.png", { frameWidth: 145, frameHeight: 144 });
-
+        this.load.spritesheet('cloud', "/battle-in-progress/cloudsheet.png", { frameWidth: 160, frameHeight: 145 });
         this.load.image('bg', "/battle-in-progress/battle-arena.png");
         if (this.player != "" && this.enemy != "") {
             this.load.image('player', this.player);
@@ -70,26 +57,17 @@ export default class Demo3Scene extends Phaser.Scene {
             });
             player.setScale(1.8);
             enemy.setScale(1.8);
+ 
 
             this.anims.create({
                 key: 'explode',
-                frames: [
-                    { key: 'cloud-1' },
-                    { key: 'cloud-2' },
-                    { key: 'cloud-3' },
-                    { key: 'cloud-4' },
-                    { key: 'cloud-5' },
-                    { key: 'cloud-6' },
-                    { key: 'cloud-7' },
-                    { key: 'cloud-8' },
-                    { key: 'cloud-9' },
-                    { key: 'cloud-10' },
-                    { key: 'cloud-11' },
-                    { key: 'cloud-12' },
-                ],
-                frameRate: 60,
+                frames: this.anims.generateFrameNames('cloud', {
+                  start: 0,
+                  end: 11,
+                }),
+                frameRate: 22,
                 repeat: -1
-            });
+              });
 
             this.physics.world.enable(this.playerFighting);
             this.playerFighting.body.setCollideWorldBounds(true);
@@ -97,10 +75,10 @@ export default class Demo3Scene extends Phaser.Scene {
             this.enemyFighting.body.setCollideWorldBounds(true);
             window.playerFighting = this.playerFighting;
             window.enemyFighting = this.enemyFighting;
-
-            window.cloud = window.gamex.add.sprite(295, 210, 'cloud');
+ 
+            window.cloud = this.add.sprite(295, 210, 'cloud');
             window.cloud.setScale(0.8);
-            window.explodeAnim = window.cloud.play("explode", false);
+            window.explodeAnim = window.cloud.play("explode");
             window.cloud.setVisible(false);
 
             this.physics.add.overlap(this.playerFighting, this.enemyFighting, function (gameObject1, gameObject2) {
